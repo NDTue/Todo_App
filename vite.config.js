311c358bf35c76a7fc9abbe0ec568e64748dad31
NDTue <ductue2003@gmail.com>
2025-04-05 01:46:05 +0700

    Add Todo App using ReactJS

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Todo_App/',// Thay 'Todo_App' bằng tên kho lưu trữ của bạn
+  base: '/Todo_App/', // Thay 'Todo_App' bằng tên kho lưu trữ của bạn
 })
